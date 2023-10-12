47a86d3af2bb39f69ca776798ae970bc49996795
Felix Sánchez <felixsanchez0124@gmail.com>
2023-10-12 15:35:52 -0500

    docstrings

--- ict-log-parser/tree_log.py
+++ ict-log-parser/tree_log.py
@@ -1,12 +1,46 @@
 from anytree import Node
 
 class TreeLog:
+    """
+    A parser class to convert a structured log file into a tree hierarchy.
+    
+    Attributes
+    ----------
+    log_str : str
+        The content of the log file read from the given path.
+
+    Methods
+    -------
+    parse(parent_node: Node = None) -> str
+        Parses the log string and constructs the tree structure.
+    """
+
     def __init__(self, path: str) -> None:
+        """
+        Initializes the TreeLog with the content of the log file.
+
+        Parameters
+        ----------
+        path : str
+            The file path of the log file to be parsed.
+        """
         with open(path, 'r') as f:
             self.log_str = f.read()
 
     def _extract_node_name(self, log_str: str):
-        """Extract the node name from the log string."""
+        """
+        Extracts the name of the node from a log string segment.
+        
+        Parameters
+        ----------
+        log_str : str
+            A segment of the log string.
+        
+        Returns
+        -------
+        tuple
+            Tuple containing node name and the remaining part of the log string.
+        """
         node_name = ""
         while len(log_str) > 0 and log_str[0] != '|':
             node_name += log_str[0]
@@ -14,15 +48,41 @@ class TreeLog:
         return node_name, log_str[1:]
 
     def _create_unique_node(self, node_name: str, parent_node: Node, sibling_count: dict) -> Node:
-        """Create a unique node based on the node name and sibling count."""
+        """
+        Creates a unique tree node based on the node name and the count of its siblings.
+        
+        Parameters
+        ----------
+        node_name : str
+            Name of the node to be created.
+        parent_node : Node
+            The parent node of the node being created.
+        sibling_count : dict
+            A dictionary to keep track of the count of sibling nodes.
+        
+        Returns
+        -------
+        Node
+            A new unique Node.
+        """
         count = sibling_count.get(node_name, 0)
         unique_node_name = f"{node_name}_{count}" if count > 0 else node_name
         sibling_count[node_name] = count + 1
         return Node(unique_node_name, parent=parent_node, data="")
 
-    def parse(self, parent_node: Node = None) -> str:
+    def build_tree(self, parent_node: Node = None) -> str:
         """
-        Parse a log string into a tree structure.
+        builds a nested tree from the parent node using the remaining log_str.
+        
+        Parameters
+        ----------
+        parent_node : Node, optional
+            The parent node for the current log string segment. Defaults to None.
+        
+        Returns
+        -------
+        str
+            Remaining log string after parsing.
         """
         sibling_count = {}  # Reset count for each new set of sibling nodes
         buffer = ""
@@ -34,7 +94,7 @@ class TreeLog:
             if char == '{':
                 node_name, self.log_str = self._extract_node_name(self.log_str)
                 node = self._create_unique_node(node_name, parent_node, sibling_count)
-                self.log_str = self.parse(node)
+                self.log_str = self.build_tree(node) # Updates the remaining log_str and recursively keeps building the tree
 
             elif char == '}':
                 if buffer:
